687016c4ff656b318bd09727a8b6bb446392693d
Raphael@Linux <akekahn@gmail.com>
2013-07-10 23:28:02 -0400

    Use html as source for app.

--- web.js
+++ web.js
@@ -2,11 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+	var buf = new Buffer();
+	buf = fs.readFileSync('index.html');
+	response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
